d9e971c54626f3f1ec1fed9de071de7a1c5a4af6
mohith-kumar28 <mohithkumar808@gmail.com>
2025-02-16 21:15:24 +0530

    feat(Sidebar): update SidebarTrigger styles with background color and rounded corners for improved visibility

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -271,7 +271,10 @@ const SidebarTrigger = React.forwardRef<
       data-sidebar='trigger'
       variant='default'
       size='icon'
-      className={cn('h-7 w-7', className)}
+      className={cn(
+        'h-7 w-7 rounded-lg bg-red-700 hover:bg-red-800',
+        className
+      )}
       onClick={(event) => {
         onClick?.(event);
         toggleSidebar();
